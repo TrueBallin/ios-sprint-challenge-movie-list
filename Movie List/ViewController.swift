70c776df88b929b6fd71cb7854140f9ae88d2267
TrueBallin <ballin_true@yahoo.com>
2020-02-07 20:50:13 -0800

    Movie App

---
+++ Movie List/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Movie List
+//
+//  Created by True Ballin on 2/7/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    func tableView(_: UIStoryboard, cellForRowAt indexPath: IndexPath) -> UILabel {
+        let cell = UILabel(withIdentifier: "MovieString", style: IndexPath)
+        
+    
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    
+
+    
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if s
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+}
